868fbc7f932f104f0980674ba357b318144f4b72
Vinh Thanh Vu <vtv@vinh.com>
2015-08-30 22:38:56 -0400

    Connecting Triggered Segues
    Action <=> show ViewController

--- experiment/ViewController.swift
+++ experiment/ViewController.swift
@@ -12,18 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-   
-    
-    @IBAction func experiment(){
         
-        // Present the image picker
-        let controller = UIImagePickerController()
-        self.presentViewController(controller, animated: true, completion:nil)
     }
-
+    @IBAction func experiment() {
+        let nextController = UIImagePickerController()  //reference
+        self.presentViewController(nextController, animated: true,
+            completion:nil)
+    }
 
 }
-
